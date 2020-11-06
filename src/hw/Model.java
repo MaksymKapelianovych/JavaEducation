9596a8c51d833c959e3219290970067ad236bccb
MaksymKapelianovych <vfrc.zomby30@gmail.com>
2020-11-06 12:53:59 +0200

    Implement View and Model

--- src/hw/Model.java
+++ src/hw/Model.java
@@ -1,4 +1,7 @@
 package hw;
 
 public class Model {
+    public static final String HELLO = "Hello";
+    public static final String WORLD = "world";
+
 }
--- src/hw/View.java
+++ src/hw/View.java
@@ -1,4 +1,12 @@
 package hw;
 
 public class View {
+    public static final String INPUT_FIRST_WORD = "Input first word 'Hello':";
+    public static final String INPUT_SECOND_WORD = "Input second word 'world':";
+    public static final String WRONG_INPUT_WORD = "The word is not correct! Try again!";
+    public static final String OUTPUT_RESULT = "Result: ";
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
 }
