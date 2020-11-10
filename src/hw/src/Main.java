fedb5896c425bb6ed71451dab04a3b9c1678a413
MaksymKapelianovych <vfrc.zomby30@gmail.com>
2020-11-10 16:46:36 +0200

    Delete other files

--- src/hw/src/Main.java
+++
@@ -1,9 +0,0 @@
-package hw.src;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    Controller controller = new Controller(new Model(), new View());
-        controller.workMethod();
-    }
-}
